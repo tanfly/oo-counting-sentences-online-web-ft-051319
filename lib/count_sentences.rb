96bace3bc3cf2874c8dae800bac5dad70de5779e
Stella McEwen <mcewen.stella@gmail.com>
2019-05-22 19:12:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,11 @@ class String
 
    def count_sentences
     self.squeeze(".", "?", "!")
+<<<<<<< HEAD
     self.split(/[.!?]/).reject {|s| s.empty?}.count
+=======
+    self.split(/[$.|?|!]/).count
+    binding.pry
+>>>>>>> 764832018a12b10de9cb465dfe662a1e8585974a
  end
 end
