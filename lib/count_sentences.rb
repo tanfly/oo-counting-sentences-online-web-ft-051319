23052c3bba612dbf5eb41564efba34810660323a
Stella McEwen <mcewen.stella@gmail.com>
2019-05-22 19:11:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?"."
   end
 
   def question?
-
+    self.end_with?"?"
   end
 
   def exclamation?
+    self.end_with?"!"
 
   end
 
-  def count_sentences
-
-  end
+   def count_sentences
+    self.squeeze(".", "?", "!")
+    self.split(/[.!?]/).reject {|s| s.empty?}.count
+ end
 end
